20d733139a5fae98c17ba608e4e2a374ff535002
Shehab Attia <shehabattia96@gmail.com>
2022-03-29 13:43:11 -0400

    Fixed joints not using the correct landmark name. Changed blenderTransformLandmarkOntoAnother() to use object2 as the object to transform onto object 1

--- blender/tests/gokart.py
+++ blender/tests/gokart.py
@@ -11,48 +11,59 @@ from textToBlender import shape, scene, BlenderLength, joint
 def createFeature(name, feature):
   source = feature["source"]  
   dimensions = feature["dimensions"]
+  initialRotation = feature["initialRotation"]
 
-  shape(name).primitive(source, dimensions) \
-    .rotate("0,90d,0")
+  shape(name).primitive(source, dimensions)
+  
+  if initialRotation:
+    shape(name).rotate(initialRotation)
 
   landmarks = feature["landmarks"]
   landmarks_split = [landmark.split(",") for landmark in landmarks]
 
   for landmark in landmarks_split:
     shape(name).landmark(landmark[0], landmark[1:])
-# scene().setDefaultUnit(BlenderLength.INCHES)
+    
+scene().setDefaultUnit(BlenderLength.INCHES)
 
-# sprocket = {
-#   "source": str(Path(__file__).parent.absolute()) + "/testFiles/6280K267_Roller Chain Sprocket.stl"
-#   }
-features = {
-  "Half Axle rod": {
+axleRodParts = {
+  "cap": {
     "source": "cylinder",
-    "dimensions": "3/8,21, in",
-    "landmarks": [
-        "teethBegin,min+1,center,center,in",
-        "teethEnd,min+2.5,center,center,in",
-        "bearingEnd, min+7,center,center,in",
-        "breakDiscStart,min+11,center,center,in",
-        "breakDiscEnd,min+15,center,center,in"
-    ]
+    "dimensions": "3/8,1,in",
+    "initialRotation": "0,90d,0",
+    "landmarks": ["left,min,center,center", "right,max,center,center"]
   },
-  # "Axle teeth": {
-  #   # "source": "teeth",
-  #   "source": "cylinder",
-  #   "dimensions": "1,1.5,in",
-  #   "landmarks": ["left,min,center,center,in"],
-  # },
-  # "bearing rod": {
-  #   "source": "cylinder",
-  #   "dimensions": "1.15,4.5,in",
-  #   "landmarks": ["left,min,center,center,in"]
-  # },
-  # "breakdisc rod": {
-  #   "source": "cylinder",
-  #   "dimensions": "1.35,14,in",
-  #   "landmarks": ["left,min,center,center,in"],
-  # },
+  "hubTeeth": {
+    # "source": "teeth",
+    "source": "cylinder",
+    "dimensions": "1,1.5,in",
+    "initialRotation": "0,90d,0",
+    "landmarks": ["left,min,center,center", "right,max,center,center"]
+  },
+  "bearing": {
+    "source": "cylinder",
+    "dimensions": "1.15,4.5,in",
+    "initialRotation": "0,90d,0",
+    "landmarks": ["left,min,center,center", "right,max,center,center"]
+  },
+  "breakdisc": {
+    "source": "cylinder",
+    "dimensions": "1.35,14,in",
+    "initialRotation": "0,90d,0",
+    "landmarks": ["left,min,center,center", "right,max,center,center"]
+  }
+}
+
+previousPart = None
+for axleRodPart in axleRodParts:
+  print("on axle rod", axleRodPart)
+  createFeature(axleRodPart, axleRodParts[axleRodPart])
+  if previousPart != None:
+    print("on axle rod joint", previousPart , " right to ", axleRodPart, " left")
+    joint(previousPart, axleRodPart, "right", "left").transformLandmarkOntoAnother()
+  previousPart = axleRodPart
+
+
   # "breakdisc core": {
   #   "source": "cylinder",
   #   "dimensions": "29/32,1,in",
@@ -84,59 +95,12 @@ features = {
   # "breakdisc right": {
   #   "source": "mirror (breakdisc left)",
   # }
-}
+# }
 
-for feature in features:
-  createFeature(feature, features[feature])
+# for feature in features:
+#   createFeature(feature, features[feature])
 
-joints = {
-  
-  "Half Axle rod with teeth": {
-    "source": "union (half axle rod, axle teeth)",
-    "dimensions": "21,3/8,3/8, in",
-  },
-  "Half Axle rod with teeth and bearing rod": {
-    "source": "union (Half Axle rod with teeth, bearing rod)",
-    "dimensions": "21,3/8,3/8, in",
-  },
-  "Half Axle rod with teeth and bearing rod and breakdisc rod": {
-    "source": "union (Half Axle rod with teeth and bearing rod, breakdisc rod)",
-    "dimensions": "21,3/8,3/8, in",
-    "landmarks": ["teethBegin,1,center,center,in",
-    "teethEnd,2.5,center,center,in",
-    "bearingEnd, 7,center,center,in",
-    "breakDiscStart,11,center,center,in",
-    "breakDiscEnd,15,center,center,in"
-    ]
-  },
-  "Axle rod": {
-    "source": "mirror (Half Axle rod with teeth and bearing rod and breakdisc rod)",
-    "landmarks": ["mirrored",
-    "mirrored",
-    "mirrored",
-    "mirrored",
-    "mirrored"]
-  },
-
-}
-
+# sprocket = {
+#   "source": str(Path(__file__).parent.absolute()) + "/testFiles/6280K267_Roller Chain Sprocket.stl"
+#   }
 # shape("sprocket").fromFile(sprocket["source"]).scale(",3in,")
-
-# shape("Half Axle rod").primitive("cylinder", features["Half Axle rod"]["dimensions"]).landmark("top", "center,center,max").rotate(["10d","20d",0])
-# shape("Half Axle rod2").primitive("cylinder", features["Half Axle rod"]["dimensions"]).landmark("bottom", "center,center,min").translate([0,0,30])
-
-# joint("Half Axle rod2", "Half Axle rod", "bottom", "top").transformLandmarkOntoAnother()
-# shape("Axle teeth").primitive("cube", features["Axle teeth"]["dimensions"])
-# shape("bearing rod").primitive("cube", features["bearing rod"]["dimensions"])
-# shape("breakdisc rod").primitive("cube", features["breakdisc rod"]["dimensions"])
-# shape("Half Axle rod with teeth").primitive("cube", features["Half Axle rod with teeth"]["dimensions"])
-# shape("Half Axle rod with teeth and bearing rod").primitive("cube", features["Half Axle rod with teeth and bearing rod"]["dimensions"])
-# shape("Half Axle rod with teeth and bearing rod and breakdisc rod").primitive("cube", features["Half Axle rod with teeth and bearing rod and breakdisc rod"]["dimensions"])
-# shape("Axle rod")
-# shape("breakdisc core").primitive("cube", features["breakdisc core"]["dimensions"])
-# shape("breakdisc mount core").primitive("cube", features["breakdisc mount core"]["dimensions"])
-# shape("breakdisc mount hole").primitive("cube", features["breakdisc mount hole"]["dimensions"])
-# shape("breakdisc mount holes")
-# shape("breakdisc mount")
-# shape("breakdisc left")
-# shape("breakdisc right")
