5623ae578dd068af8d73c91f8e9afbabfd2f7917
Shehab Attia <shehabattia96@gmail.com>
2022-05-23 19:09:54 -0400

    Curves now get transformed into a mesh before boolean operations. NOTE: this might be a temporary step until we sort out the capabilities on each mesh type

--- blender/tests/stackableLeg.py
+++ blender/tests/stackableLeg.py
@@ -4,47 +4,47 @@ sys.path.append(str(Path(__file__).parent.parent.absolute()))
 
 from textToBlender import *
 
-# scene().setDefaultUnit(BlenderLength.MILLIMETERS)
+scene().setDefaultUnit(BlenderLength.MILLIMETERS)
 
-# curve("legExtrudePath").createLine("5in").rotate("0,0,90d")
-# curve("stackingCutoutExtrudePath").createLine("1in").rotate("0,0,90d")
+curve("legExtrudePath").createLine("5in").rotate("0,0,90d")
+curve("stackingCutoutExtrudePath").createLine("1in").rotate("0,0,90d")
 
-# curve("ellipseLeg").createEllipse("14mm", "27mm") \
-#     .sweep("extrudePath", False) \
-#     .thicken("5mm") \
-#     .landmark("top","center,max,center")\
-#     .landmark("bottom","center,min,center")
+curve("ellipseLeg").createEllipse("14mm", "27mm") \
+    .sweep("legExtrudePath", False) \
+    .thicken("5mm") \
+    .landmark("top","center,center,max")\
+    .landmark("bottom","center,center,min")
 
-# curve("ellipseLegOuterCutout").createEllipse("14+3mm", "27+3mm") \
-#     .sweep("stackingCutoutExtrudePath", False) \
-#     .thicken("5mm") \
-#     .landmark("bottom","center,min,center")
+curve("ellipseLegOuterCutout").createEllipse("14+3mm", "27+3mm") \
+    .sweep("stackingCutoutExtrudePath", False) \
+    .thicken("5mm") \
+    .landmark("top","center,center,max")
 
-# joint("ellipseLeg", "ellipseLegOuterCutout", "bottom", "bottom").transformLandmarkOntoAnother()
+joint("ellipseLeg", "ellipseLegOuterCutout", "top", "top").transformLandmarkOntoAnother()
 
-# curve("ellipseLeg").subtract("ellipseLegOuterCutout")
+curve("ellipseLeg").subtract("ellipseLegOuterCutout")
 
-shape("ellipseLeg").primitive("cylinder", "1,1,1").remesh().apply()
-shape("ellipseLeg").scale("20+3mm,34+3mm,5in")
-shape("ellipseLegInner").primitive("cylinder", "1,1,1").remesh().apply()
-shape("ellipseLegInner").scale("20mm,34mm,6in")
+# shape("ellipseLeg").primitive("cylinder", "1,1,1").remesh().apply()
+# shape("ellipseLeg").scale("20+3mm,34+3mm,5in")
+# shape("ellipseLegInner").primitive("cylinder", "1,1,1").remesh().apply()
+# shape("ellipseLegInner").scale("20mm,34mm,6in")
 
-shape("ellipseLeg").subtract("ellipseLegInner")
-shape("ellipseLeg").landmark("top", "center,center,max")
+# shape("ellipseLeg").subtract("ellipseLegInner")
+# shape("ellipseLeg").landmark("top", "center,center,max")
 
-scene().setShapeVisibility("ellipseLegInner", False)
-shape("ellipseLegCutout").primitive("cylinder", "1,1,1")
-shape("ellipseLegCutout").scale("20+5mm,34+5mm,1in")
-shape("ellipseLegCutoutInner").primitive("cylinder", "1,1,1")
-shape("ellipseLegCutoutInner").scale("20+2mm,34+2mm,2in")
+# scene().setShapeVisibility("ellipseLegInner", False)
+# shape("ellipseLegCutout").primitive("cylinder", "1,1,1")
+# shape("ellipseLegCutout").scale("20+5mm,34+5mm,1in")
+# shape("ellipseLegCutoutInner").primitive("cylinder", "1,1,1")
+# shape("ellipseLegCutoutInner").scale("20+2mm,34+2mm,2in")
 
-shape("ellipseLegCutout").subtract("ellipseLegCutoutInner").apply()
-shape("ellipseLegCutout").landmark("top", "center,center,max")
+# shape("ellipseLegCutout").subtract("ellipseLegCutoutInner").apply()
+# shape("ellipseLegCutout").landmark("top", "center,center,max")
 
-scene().setShapeVisibility("ellipseLegCutoutInner", False)
+# scene().setShapeVisibility("ellipseLegCutoutInner", False)
 
-joint("ellipseLeg", "ellipseLegCutout", "top", "top").transformLandmarkOntoAnother()
+# joint("ellipseLeg", "ellipseLegCutout", "top", "top").transformLandmarkOntoAnother()
 
-shape("ellipseLeg").subtract("ellipseLegCutout")
+# shape("ellipseLeg").subtract("ellipseLegCutout")
 
-scene().setShapeVisibility("ellipseLegCutout", False)
+# scene().setShapeVisibility("ellipseLegCutout", False)
