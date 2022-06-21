755b080f348143521695f8681927d0282995f2cf
Shehab Attia <shehabattia96@gmail.com>
2022-06-21 15:15:14 -0400

    Fixed the text example, added a font that supports Japanese unicode. Fixed a bug with script imports.

--- tests/test_textToBlender.py
+++ tests/test_textToBlender.py
@@ -1,9 +1,3 @@
-import sys
-from pathlib import Path
-scriptDir = Path(__file__).parent.parent.absolute()
-if scriptDir not in sys.path:
-    sys.path.insert(0, str(scriptDir))
-
 def testPrimitive():
     import bpy
     from CodeToCADBlenderProvider import shape
