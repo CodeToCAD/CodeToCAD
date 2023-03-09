2ab86ce08188219362f9840d2f314167156a0174
Shehab Attia <shehab@meadowsdesign.com>
2023-03-08 23:37:50 -0500

    Removes virtual environment files that were not supposed to be committed..

--- development/developmentVirutalEnvironment/lib/python3.11/site-packages/bl_ui_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-import typing
-from . import bug_report_url
-
-GenericType = typing.TypeVar("GenericType")
--- development/developmentVirutalEnvironment/lib/python3.11/site-packages/bpy_extras/wm_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-import typing
-from . import progress_report
-
-GenericType = typing.TypeVar("GenericType")
