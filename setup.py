fdbcc169968abadfac71f53b0af82ad80ce3b304
Shehab Attia <shehab@meadowsdesign.com>
2023-03-14 14:47:38 -0400

    Updated README to use github as image provider for image reference links.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='CodeToCAD',
-    version='0.2.5',
+    version='0.2.7',
     description='3D modeling automation in your favorite modeling software.',
     long_description=long_description,
     long_description_content_type='text/markdown',
