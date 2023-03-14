7a49a38c0acd32004917efc00e4f4fd72060f1ef
Shehab Attia <shehab@meadowsdesign.com>
2023-03-14 14:07:28 -0400

    Renamed core -> CodeToCAD. providers/CodeToCAD.py is not CodeToCAD/__init__.py. Added setup.py to create a CodeToCAD distributed package. Refactored project to use CodeToCAD instead of core.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='CodeToCAD',
+    version='0.2.4',
+    description='3D modeling automation in your favorite modeling software.',
+    long_description='Intuitive code-based CAD that enforces good modeling practices. Models are automatically generated in existing CAD or modeling software.',
+    long_description_content_type='text/x-rst',
+    url='https://github.com/CodeToCAD/CodeToCAD',
+    author='CodeToCAD',
+    author_email='shehab@codethatdown.com',
+    license='GPL v3',
+    packages=['CodeToCAD'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
+    ],
+)
