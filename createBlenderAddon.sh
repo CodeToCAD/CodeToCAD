e09871adf470f17ec8e8c104b7cc8a6947491e61
Shehab Attia <shehabattia96@gmail.com>
2023-03-20 14:42:36 -0400

    Attempt to fix push workflow.

--- createBlenderAddon.sh
+++ createBlenderAddon.sh
@@ -7,7 +7,7 @@ cp -r ./providers/blender $BLENDER_ADDON_PATH
 cp -r ./CodeToCAD $BLENDER_ADDON_PATH/
 
 mv -f $BLENDER_ADDON_PATH/CodeToCADBlenderAddon.py $BLENDER_ADDON_PATH/__init__.py
-GIT_EPOCH=$(git show -s --format=%ct HEAD~)
+GIT_EPOCH=$(git show -s --format=%ct HEAD)
 sed -i '' "s/),  # patch_version marker do not remove/, $GIT_EPOCH),/g" $BLENDER_ADDON_PATH/__init__.py
 
 rm CodeToCADBlenderAddon.zip
