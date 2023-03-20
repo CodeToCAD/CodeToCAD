7ea29cd15dc6d84f7b4be39bffb2fc34d34526ff
Shehab Attia <shehabattia96@gmail.com>
2023-03-20 14:56:20 -0400

    Fixed createBlenderAddon script sed parameter for linux, added shebangs.

--- createBlenderAddon.sh
+++ createBlenderAddon.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 BLENDER_ADDON_PATH="./CodeToCADBlenderAddon/"
 
@@ -8,7 +9,12 @@ cp -r ./CodeToCAD $BLENDER_ADDON_PATH/
 
 mv -f $BLENDER_ADDON_PATH/CodeToCADBlenderAddon.py $BLENDER_ADDON_PATH/__init__.py
 GIT_EPOCH=$(git show -s --format=%ct HEAD)
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
 sed -i '' "s/),  # patch_version marker do not remove/, $GIT_EPOCH),/g" $BLENDER_ADDON_PATH/__init__.py
+else
+sed "s/),  # patch_version marker do not remove/, $GIT_EPOCH),/g" $BLENDER_ADDON_PATH/__init__.py
+fi
 
 rm CodeToCADBlenderAddon.zip
 
