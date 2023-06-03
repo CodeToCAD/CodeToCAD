432716936741afbea92decf101dd0b008e3dd60f
Shehab Attia <shehabattia96@gmail.com>
2023-06-03 18:58:13 -0400

    Added the ability to add new Part Studio tabs. Added onshapeUrl formatters for recurring onshape-client dictionary. Added the ability to add a point to the Scene.

--- providers/onshape/OnshapeActions.py
+++ providers/onshape/OnshapeActions.py
@@ -1,6 +1,21 @@
 import CodeToCAD.utilities as Utilities
 import OnshapeDefinitions
-from onshape_client import OnshapeElement, Client
+from onshape_client import Client
+from onshape_client.oas import (
+    BTMParameterString149,
+    BTMSketch151,
+    BTMSketchConstraint2,
+    BTMSketchCurveSegment155,
+    BTMIndividualQuery138,
+    BTMParameterQueryList148,
+    BTMSketchPoint158,
+    BTCurveGeometryLine117,
+    BTMSketchCurve4,
+    BTCurveGeometryCircle115,
+    BTModelElementParams,
+    BTFeatureDefinitionCall1406,
+    BTMParameterQuantity147
+)
 
 
 def getOnshapeClient(config: dict) -> Client:
@@ -42,3 +57,41 @@ def getFirstDocumentUrlById(client: Client, documentId: str) -> OnshapeDefinitio
 def getFirstDocumentUrlByName(client: Client, documentName: str) -> OnshapeDefinitions.OnshapeUrl:
     documentId = getDocumentByName(client, documentName)["id"]
     return getFirstDocumentUrlById(client, documentId)
+
+
+def createTabPartStudios(client: Client, onshapeUrl: OnshapeDefinitions.OnshapeUrl, tabName: str) -> str:
+    '''
+    Create a Part Studio tab and return the newly created tab id
+    '''
+
+    partStudio = client.part_studios_api.create_part_studio(
+        **onshapeUrl.dictDocumentAndWorkspace, bt_model_element_params=BTModelElementParams(name=tabName))
+    return partStudio["id"]
+
+
+def createSketch(client: Client, onshapeUrl: OnshapeDefinitions.OnshapeUrl, sketchName: str, btmEntities: list):
+    # References https://github.com/onshape-public/onshape-clients/blob/master/python/test/test_part_studios_api.py
+    PLANE_ID = "JDC"  # The plane deterministic ID for the sketch
+    plane_query = BTMParameterQueryList148(
+        parameter_id="sketchPlane",
+        queries=[BTMIndividualQuery138(deterministic_ids=[PLANE_ID])],
+    )
+    sketch = BTMSketch151(
+        entities=btmEntities, name=sketchName, parameters=[plane_query]
+    )
+    feature_definition = BTFeatureDefinitionCall1406(
+        feature=sketch, bt_type="BTFeatureDefinitionCall-1406"
+    )
+    return client.part_studios_api.add_part_studio_feature(
+        **onshapeUrl.dictDocumentAndWorkspaceAndModelAndTab,
+        bt_feature_definition_call_1406=feature_definition,
+        _preload_content=False,
+    )
+
+
+def createPoint(client: Client, onshapeUrl: OnshapeDefinitions.OnshapeUrl, sketchName: str,  point: Utilities.Point):
+    btmPoint = BTMSketchPoint158(
+        y=point.y.value, x=point.x.value, is_user_point=True, is_construction=False, parameters=[]
+    )
+
+    return createSketch(client, onshapeUrl, sketchName=sketchName, btmEntities=[btmPoint])
