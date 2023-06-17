8a79a9454ff2c5ef5123e2e651d88ee1ce90743e
Shehab Attia <shehabattia96@gmail.com>
2023-06-17 14:40:13 -0400

    Added an example and render for coil.py. Moved doc images to /docs/images, and updated references. Added an index.md in docs.

---
+++ examples/coil.py
@@ -0,0 +1,15 @@
+from CodeToCAD import *
+
+
+drumRadius = Dimension.fromString("5mm")
+drumLength = Dimension.fromString("15.0 mm")
+
+wireThickness = Dimension.fromString("1mm")
+numberOfWindings = 10
+
+grooveSpiral = Sketch("grooveSpiral").createSpiral(
+    numberOfWindings, drumLength, drumRadius)
+
+groove = Sketch("groove").createCircle(wireThickness/2)
+
+grooveSpiral.sweep(groove)
