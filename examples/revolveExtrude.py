ff02f6ea208d71a9d2337cd6dba77412c9773b23
Shehab Attia <shehabattia96@gmail.com>
2023-09-29 14:19:47 -0400

    Curve -> Sketch

--- examples/revolveExtrude.py
+++ examples/revolveExtrude.py
@@ -1,7 +1,7 @@
 from CodeToCAD import *
 
-Curve("centerline").createLine("1")
-Curve("half-donut").createCircle("10cm").translateXYZ("-100cm",
-                                                      0, 0).revolve("180d", "centerline",  "y")
+Sketch("centerline").createLine("1")
+Sketch("half-donut").createCircle("10cm").translateXYZ("-100cm",
+                                                       0, 0).revolve("180d", "centerline",  "y")
 
-Curve("ribbon").createArc("1cm", "45d").twist("180d", "2.5cm", 100, "z")
+Sketch("ribbon").createArc("1cm", "45d").twist("180d", "2.5cm", 100, "z")
--- examples/text.py
+++ examples/text.py
@@ -5,25 +5,25 @@ redMaterial = Material("red").setColor(0.709804, 0.109394, 0.245126, 1.0)
 greenMaterial = Material("green").setColor(0.118213, 0.709804, 0.109477, 0.8)
 blueMaterial = Material("blue").setColor(0.0865257, 0.102776, 0.709804, 0.8)
 
-Curve("CodeToCAD").createText("Code To CAD",
-                              underlined=True).extrude("1cm").setMaterial(greenMaterial)
+Sketch("CodeToCAD").createText("Code To CAD",
+                               underlined=True).extrude("1cm").setMaterial(greenMaterial)
 
-Curve("Small text!").createText("Small text!",
-                                fontSize="15cm", underlined=True).translateXYZ(0, -2, 0)
+Sketch("Small text!").createText("Small text!",
+                                 fontSize="15cm", underlined=True).translateXYZ(0, -2, 0)
 
 arialFontPath = str(Path(__file__).parent.absolute()) + "/fonts/arial.ttf"
-Curve("كود تو كاد").createText("كود تو كاد",
-                               fontFilePath=arialFontPath).translateXYZ(-5, 0, 0)
+Sketch("كود تو كاد").createText("كود تو كاد",
+                                fontFilePath=arialFontPath).translateXYZ(-5, 0, 0)
 
 
 yuGothicFontPath = str(Path(__file__).parent.absolute()
                        ) + "/fonts/yu_gothic.ttc"
-Curve("curvedTextPath").createCircle(
+Sketch("curvedTextPath").createCircle(
     "1m").translateXYZ(-5, -5, 0).setVisible(False)
-Curve("コオデツカアド").createText("コオデツカアド", fontFilePath=yuGothicFontPath).profile(
+Sketch("コオデツカアド").createText("コオデツカアド", fontFilePath=yuGothicFontPath).profile(
     "curvedTextPath").translateXYZ(-5, -5, 0).extrude(.1).setMaterial(redMaterial)
 
-Curve("multiline").createText(
+Sketch("multiline").createText(
     """Multiline
     Text
         Is
