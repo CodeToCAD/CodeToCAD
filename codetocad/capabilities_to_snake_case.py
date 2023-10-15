522b996eabe444517e421644780b9608d32add0a
Shehab Attia <shehabattia96@gmail.com>
2023-10-15 15:23:04 -0400

    All methods and parameters are now snake_case in capabilities.json

---
+++ codetocad/capabilities_to_snake_case.py
@@ -0,0 +1,51 @@
+import re
+import json
+pattern1 = re.compile(r'(.)([A-Z][a-z]+)')
+pattern2 = re.compile(r'__([A-Z])')
+pattern3 = re.compile(r'([a-z0-9])([A-Z])')
+
+
+def to_snake_case(name):
+    # references https://stackoverflow.com/a/1176023/9824103
+    name = pattern1.sub(r'\1_\2', name)
+    name = pattern2.sub(r'_\1', name)
+    name = pattern3.sub(r'\1_\2', name)
+    return name.lower()
+
+
+capabilities = json.load(
+    open("/Users/shehab/Desktop/projects/CodeToCAD/codetocad/capabilities.json"))
+
+# capabilities_new = json.loads(json.dumps(capabilities))
+
+for className in capabilities["capabilities"].keys():
+    methods = list(capabilities["capabilities"][className].keys())
+
+    for method in methods:
+
+        method_name_snake = to_snake_case(method)
+
+        if method_name_snake != method:
+            capabilities["capabilities"][className][method_name_snake] = capabilities["capabilities"][className][method]
+
+            del capabilities["capabilities"][className][method]
+
+        if "parameters" in capabilities["capabilities"][className][method_name_snake]:
+
+            parameters = list(capabilities["capabilities"][className][method_name_snake]["parameters"].keys(
+            ))
+
+            for parameter in parameters:
+                parameter_name_snake = to_snake_case(parameter)
+
+                if parameter_name_snake == parameter:
+                    continue
+
+                capabilities["capabilities"][className][method_name_snake]["parameters"][
+                    parameter_name_snake] = capabilities["capabilities"][className][method_name_snake]["parameters"][parameter]
+
+                del capabilities["capabilities"][className][method_name_snake]["parameters"][parameter]
+
+
+json.dump(capabilities, open(
+    "/Users/shehab/Desktop/projects/CodeToCAD/codetocad/capabilities_snake.json", "w"))
