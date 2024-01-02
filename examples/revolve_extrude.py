fdf2ecbb13dd72abc30f5280411e3b20a4abbe47
Shehab Attia <shehabattia96@gmail.com>
2024-01-01 22:50:16 -0500

    Fixed errors in sketching examples.

--- examples/revolve_extrude.py
+++ examples/revolve_extrude.py
@@ -1,8 +1,13 @@
 from codetocad import *
 
-Sketch("centerline").create_line("1")
-Sketch("half-donut").create_circle("10cm").translate_xyz("-100cm", 0, 0).revolve(
+Sketch("centerline").create_line([0, 0, 0], [0, 1, 0])
+
+half_donut = Sketch("half-donut")
+half_donut.create_circle("10cm")
+half_donut.translate_xyz("-100cm", 0, 0).revolve(
     "180d", "centerline", "y"
 )
 
-Sketch("ribbon").create_arc("1cm", "45d").twist("180d", "2.5cm", 100, "z")
+ribbon = Sketch("ribbon")
+ribbon.create_arc([0, 0, 0], ["1cm", "1cm", 0], 0)
+ribbon.twist("180d", "2.5cm", 100, "z")
