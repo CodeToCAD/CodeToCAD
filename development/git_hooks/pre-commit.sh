73f8d3701353790fc2f43e06af07c11e680beb95
Patrick <plwilkie@gmail.com>
2024-01-03 15:45:05 -0700

    Test pre-commit formatting

--- development/git_hooks/pre-commit.sh
+++ development/git_hooks/pre-commit.sh
@@ -15,12 +15,12 @@ echo "$FILES" | xargs black
 # Add back the modified/prettified files to staging
 echo "$FILES" | xargs git add
 
-NO_EMPTY=git diff --cached --exit-code
-if [ $NO_EMPTY -eq 0 ]
-then
-    echo "Black formatting resulted in an empty commit, cancelling!"
-    exit 1
-fi
+# NO_EMPTY=git diff --cached --exit-code
+# if [ $NO_EMPTY -eq 0 ]
+# then
+#     echo "Black formatting resulted in an empty commit, cancelling!"
+#     exit 1
+# fi
 
 # Mark: run tests  and lint checks
 sh "$ROOT_DIR/development/run_tests.sh"
