5dcf22aa0d51848cabb39a48d3e8062ae890e9d0
Vallahor <vallahor91@gmail.com>
2024-01-09 15:59:39 -0300

    Add ability to offset a landmark relative to another #136 (#276)

---
+++ examples/landmark_relative.py
@@ -0,0 +1,19 @@
+from codetocad import *
+
+wall_thickness = Dimension.from_string(0.1)
+
+hollow_cube = Part("hollow_cube").create_cube(1, 1, 0.1)
+hollow_cube.hollow(wall_thickness * 2, wall_thickness * 2, 0)
+
+hollow_cube.translate_x(3)
+
+c1 = Part("c1").create_cube(1, 1, 1)
+c1.translate_x(2)
+c1.translate_y(2)
+c1.translate_z(2)
+
+hollow_cube_left = hollow_cube.get_landmark(PresetLandmark.left)
+hollow_cube_left_inner = hollow_cube_left.clone("left_inner")
+hollow_cube_left_inner = hollow_cube_left.clone("left_inner_2", [wall_thickness, 0, 0])
+
+hollow_cube_left_inner = hollow_cube_left.clone("left_inner_3", parent=c1)
