22350752ead44a9285c90828b7069221b1dba52f
Vallahor <vallahor91@gmail.com>
2024-01-08 22:06:30 -0300

    Boolean modifier transfer materials (#274)
    * Boolean modifiers transfer materials #99

---
+++ examples/union.py
@@ -0,0 +1,16 @@
+from codetocad import *
+from codetocad.enums import PresetMaterial
+
+c1 = Part("c1").create_cube(1,1,1)
+c1.set_material(PresetMaterial.blue)
+
+c2 = Part("c2").create_cylinder(0.5,1)
+c2.set_material(PresetMaterial.red)
+c2.translate_x(0.5)
+
+c3 = Part("c3").create_cube(1,1,1)
+c3.set_material(PresetMaterial.yellow)
+c3.translate_x(-0.5)
+
+c2.union(c1)
+c2.union(c3)
