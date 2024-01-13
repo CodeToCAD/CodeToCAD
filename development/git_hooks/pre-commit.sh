ee7675fe091bba4945f1e90d1b0eec6a515e59cd
Shehab Attia <shehabattia96@gmail.com>
2024-01-13 06:10:04 -0500

    run_tests.sh now runs all the testcases. Removed the exit condition in the precommit hook that was causing test and linting to not run.

--- development/git_hooks/pre-commit.sh
+++ development/git_hooks/pre-commit.sh
@@ -2,25 +2,19 @@
 
 ROOT_DIR=`git rev-parse --show-toplevel`
 
+echo "Running pre-commit hooks."
+
 set -e # exit if script errors.
 
 # Reference: https://prettier.io/docs/en/precommit.html
 # ACMR = Added, Copied, Modified, Renamed (no deleted)
 FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep '\.\(py\)$' | sed 's| |\\ |g')
-[ -z "$FILES" ] && exit 0
 
 # Prettify all selected files
 echo "$FILES" | xargs black
 
 # Add back the modified/prettified files to staging
 echo "$FILES" | xargs git add
-
-# NO_EMPTY=git diff --cached --exit-code
-# if [ $NO_EMPTY -eq 0 ]
-# then
-#     echo "Black formatting resulted in an empty commit, cancelling!"
-#     exit 1
-# fi
 
 # Mark: run tests  and lint checks
 sh "$ROOT_DIR/development/run_tests.sh"
