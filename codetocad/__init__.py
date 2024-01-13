82a5fa437237c106ccb79be85fe099d1b8578d0f
Shehab Attia <shehabattia96@gmail.com>
2024-01-13 01:07:51 -0500

    Removed find_loader and added logic for importing sample_provider. This resolved #259.

--- codetocad/__init__.py
+++ codetocad/__init__.py
@@ -2,9 +2,8 @@ from codetocad.utilities import *
 from codetocad.codetocad_types import *
 from codetocad.interfaces import *
 
-from importlib import find_loader
+from pathlib import Path
 
-if find_loader("blender_provider"):
-    from blender_provider import *
-else:
+if not Path("./providers").exists():
+    # When this python library is bundled, use the providers_sample if the "providers" directory is not copied.
     from codetocad.providers_sample import *
