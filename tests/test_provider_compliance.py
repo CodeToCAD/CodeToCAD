132c0a77c674ecd8f605a108bd669e4c5e281f62
Shehab Attia <shehabattia96@gmail.com>
2024-01-15 00:37:23 -0500

    Fixed the test_provider_compliance test because shipping providers with abstract classes is a no-go.

--- tests/test_provider_compliance.py
+++ tests/test_provider_compliance.py
@@ -1,11 +1,31 @@
+import inspect
 import unittest
+import json
+
+capabilities_json = "codetocad/capabilities.json"
+
+capabilies_class_names = []
+
+with open(capabilities_json) as f:
+    capabilities: dict = json.load(f)
+    for capability_name, value in capabilities["capabilities"].items():
+        if not value.get("is_interface_only", False):
+            capabilies_class_names.append(capability_name)
 
 
 class TestProviderCompliance(unittest.TestCase):
-    def test_blender_provider(self):
-        # NOTE: if you're running this test locally, you might need `pip install fake-bpy-module-latest` or Blender API added to python path
-        from providers.blender.blender_provider import Part, Sketch, Landmark
-
-        Part("")
-        Sketch("")
-        Landmark("", "")
+    def test_no_abstract_provider(self):
+        print("capabilies_class_names", capabilies_class_names)
+        all_providers_import = __import__(f"providers")
+        all_providers_modules = inspect.getmembers(
+            all_providers_import, predicate=inspect.ismodule
+        )
+        for module_name, provider_module in all_providers_modules:
+            all_classes = inspect.getmembers(provider_module, predicate=inspect.isclass)
+            for class_name, some_class in all_classes:
+                if class_name not in capabilies_class_names:
+                    continue
+                abstract_methods = list(some_class.__abstractmethods__)
+                assert (
+                    len(abstract_methods) == 0
+                ), f"{class_name} has abstract methods {abstract_methods} in the {module_name} provider."
