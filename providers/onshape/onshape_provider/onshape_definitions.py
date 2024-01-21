d2fb2de6e87e67a999bdf4c0a5016fe69d14146d
Shehab Attia <shehabattia96@gmail.com>
2024-01-21 00:09:40 -0500

    Updated test_onshape_actions to auto-create the test document and tab. Deduplicated code in the test file.

--- providers/onshape/onshape_provider/onshape_definitions.py
+++ providers/onshape/onshape_provider/onshape_definitions.py
@@ -3,6 +3,9 @@ class OnshapeUrl:
     # https://cad.onshape.com/documents/b5c68587473845a7c3313c68/w/df7e37f0d4d9aa712d9eb3db/e/80cf7ac7b27188a730391d61
     # document_id, workspace_id and tab_id respectively.
     # This class helps keep track and format this url.
+
+    # TODO: replace usages of this class with the onshape-client library's OnshapeElement class
+
     def __init__(
         self,
         document_id: str,
--- tests_integration/test_onshape_actions.py
+++ tests_integration/test_onshape_actions.py
@@ -13,8 +13,9 @@ configPath = os.path.join(
     "../providers/onshape/.onshape_client_config.yaml",
 )
 
-# Note: you must create a "CodeToCAD-onshape_actions" document to run tests that use it.
-onshape_document_name = "CodeToCAD-onshape_actions"
+# Note: you must create a "CodeToCAD-onshape_actions" document and "test_onshape_actions" tab to run tests that use it.
+ONSHAPE_DOCUMENT_NAME = "CodeToCAD-onshape_actions"
+ONSHAPE_TAB_NAME = "test_onshape_actions"
 
 
 class TestOnshapeActions(unittest.TestCase):
@@ -22,128 +23,158 @@ class TestOnshapeActions(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.client = get_onshape_client_with_config_file(config_filepath=configPath)
 
-        # from onshape_provider import injectonshape_provider
-        # injectonshape_provider(globals())
+        document = get_document_by_name_or_none(cls.client, ONSHAPE_DOCUMENT_NAME)
+        if document is None:
+            document = create_document(cls.client, ONSHAPE_DOCUMENT_NAME)
 
-    def setUp(self) -> None:
-        pass
+        document_id = document["id"]
 
-    def test_read_document_url_by_name(self) -> None:
-        documentUrl = get_first_document_url_by_name(self.client, onshape_document_name)
+        workspace_id = get_workspaces(cls.client, document_id)[0]["id"]
 
-        assert documentUrl is not None
+        if (
+            get_tab_by_name_or_none(
+                cls.client, document_id, workspace_id, ONSHAPE_TAB_NAME
+            )
+            is None
+        ):
+            create_tab_part_studios(
+                cls.client, document_id, workspace_id, ONSHAPE_TAB_NAME
+            )
 
-        print("documentUrl", documentUrl)
-
-    def test_create_part_studio_tab(self) -> None:
-        documentUrl = get_first_document_url_by_name(self.client, onshape_document_name)
-
-        partStudioId = create_tab_part_studios(
-            self.client, documentUrl, Utilities.create_uuid_like_id()
+        cls.onshape_url = get_tab_url_by_name(
+            cls.client, ONSHAPE_DOCUMENT_NAME, ONSHAPE_TAB_NAME
         )
-
-        assert partStudioId is not None
 
     def test_create_point(self) -> None:
-        # Replace 'CodeToCAD-onshape_actions' with the actual document name
-        onshape_document_name = "CodeToCAD-onshape_actions"
-
-        # Get the URL of the Onshape document
-        onshape_url = get_first_document_url_by_name(self.client, onshape_document_name)
-
-        # Create a new tab in the part studio
-        part_studio_id = create_tab_part_studios(
-            self.client, onshape_url, Utilities.create_uuid_like_id()
-        )
-
-        # Set the tab_id for subsequent operations
-        onshape_url.tab_id = part_studio_id
-
         # Define the location of the point in 3D space
         pointLocation = Dimension(15.0, "mm")
 
+        sketch_name = "a point"
+
         # Create a point in the part studio
-        create_point(
+        sketch_info = create_point(
             self.client,
-            onshape_url,
-            "Test Point",
+            self.onshape_url,
+            sketch_name,
             Point(pointLocation, pointLocation, pointLocation),
         )
 
+        assert sketch_info.status == 200
+
+        data = json.loads(sketch_info.data)
+
+        assert (
+            data["name"] == sketch_name
+        ), f"Unexpected sketch name: {data['name']}. Expected {sketch_name}"
+
     def test_create_rect(self) -> None:
-        # Get the URL of the Onshape document
-        onshape_url = get_first_document_url_by_name(self.client, onshape_document_name)
-
-        # Create a new tab in the part studio
-        part_studio_id = create_tab_part_studios(
-            self.client, onshape_url, Utilities.create_uuid_like_id()
-        )
-
-        # Set the tab_id for subsequent operations
-        onshape_url.tab_id = part_studio_id
-
         # Define the location of the point in 3D space
         pointLocation1 = Dimension(0.1, "inch")
         pointLocation2 = Dimension(0.3, "inch")
 
-        create_rect(
+        sketch_name = "a rectangle"
+
+        sketch_info = create_rect(
             self.client,
-            onshape_url,
-            "Test Point",
+            self.onshape_url,
+            sketch_name,
             Point(pointLocation1, pointLocation1, pointLocation1),
             Point(pointLocation2, pointLocation2, pointLocation2),
         )
 
+        assert sketch_info.status == 200
+
+        data = json.loads(sketch_info.data)
+
+        assert (
+            data["name"] == sketch_name
+        ), f"Unexpected sketch name: {data['name']}. Expected {sketch_name}"
+
     def test_create_circle(self):
-        # Get the URL of the Onshape document
-        onshape_url = get_first_document_url_by_name(self.client, onshape_document_name)
-
-        # Create a new tab in the part studio
-        part_studio_id = create_tab_part_studios(
-            self.client, onshape_url, Utilities.create_uuid_like_id()
-        )
-
-        # Set the tab_id for subsequent operations
-        onshape_url.tab_id = part_studio_id
-
         # Define the location of the point in 3D space
         pointLocation1 = Dimension(0.1, "inch")
         pointLocation2 = Dimension(0.3, "inch")
 
-        create_circle(
+        sketch_name = "a circle"
+
+        sketch_info = create_circle(
             self.client,
-            onshape_url,
-            "Test Point",
+            self.onshape_url,
+            sketch_name,
             0.05,
             Point(pointLocation1, pointLocation2, pointLocation2),
         )
 
-    def test_extrude(self):
-        onshape_document_name = "CodeToCAD-onshape_actions"
+        assert sketch_info.status == 200
 
-        # Get the URL of the Onshape document
-        onshape_url = get_first_document_url_by_name(self.client, onshape_document_name)
+        data = json.loads(sketch_info.data)
+        feature_name = data["feature"]["name"]
+        assert (
+            feature_name == sketch_name
+        ), f"Unexpected sketch name: {feature_name}. Expected {sketch_name}"
 
-        # Create a new tab in the part studio
-        part_studio_id = create_tab_part_studios(
-            self.client, onshape_url, Utilities.create_uuid_like_id()
+    def test_update_sketch(self):
+        # Define the location of the point in 3D space
+        pointLocation1 = Dimension(0.1, "inch")
+        pointLocation2 = Dimension(0.3, "inch")
+
+        sketch_name = "a circle and a rect"
+
+        sketch_info = create_circle(
+            self.client,
+            self.onshape_url,
+            sketch_name,
+            0.05,
+            Point(pointLocation1, pointLocation2, pointLocation2),
+        )
+        # sketch_info = create_rect(
+        #     self.client,
+        #     self.onshape_url,
+        #     sketch_name,
+        #     Point(pointLocation1, pointLocation1, pointLocation1),
+        #     Point(pointLocation2, pointLocation2, pointLocation2),
+        # )
+
+        assert sketch_info.status == 200
+
+        data = json.loads(sketch_info.data)
+        feature_name = data["feature"]["name"]
+        assert (
+            feature_name == sketch_name
+        ), f"Unexpected sketch name: {feature_name}. Expected {sketch_name}"
+
+        sketch_info = create_rect(
+            self.client,
+            self.onshape_url,
+            sketch_name,
+            Point(pointLocation1, pointLocation1, pointLocation1),
+            Point(pointLocation2, pointLocation2, pointLocation2),
         )
 
-        # Set the tab_id for subsequent operations
-        onshape_url.tab_id = part_studio_id
+        assert sketch_info.status == 200
 
+        data = json.loads(sketch_info.data)
+        feature_name = data["feature"]["name"]
+        assert (
+            feature_name == sketch_name
+        ), f"Unexpected sketch name: {feature_name}. Expected {sketch_name}"
+
+    def test_extrude(self):
         # Define the location of the point in 3D space
         pointLocation1 = Dimension(15.0, "mm")
         pointLocation2 = Dimension(25.0, "mm")
 
+        extrude_amount = Dimension.from_string("2in")
+
         # Create a point in the part studio
         sketch_info = create_rect(
             self.client,
-            onshape_url,
-            "Test Point",
+            self.onshape_url,
+            "Cube Sketch",
             Point(pointLocation1, pointLocation1, pointLocation1),
             Point(pointLocation2, pointLocation2, pointLocation2),
         )
 
         feature_id = json.loads(sketch_info.data)["feature"]["featureId"]
-        create_extrude(self.client, onshape_url, feature_id)
+
+        create_extrude(self.client, self.onshape_url, feature_id, f"{extrude_amount}")
