8d4c418834720774ea97dc95561e9475ad7dc852
Shehab Attia <shehabattia96@gmail.com>
2024-03-08 18:55:08 -0500

    Added a facade-factory that provides a single place to import CodeToCAD implementable classes, but is agonistic to the provider. The providers may be registered using the register() method.

--- development/capabilities_json_to_python/paths.py
+++ development/capabilities_json_to_python/paths.py
@@ -16,6 +16,9 @@ capabilities_json_path = f"{root_dir}/codetocad/capabilities.json"
 capabilities_to_py_interface = "capabilities_to_py_interface.j2"
 capabilities_to_py_interface_out = f"{codetocad_dir}/interfaces"
 
+capabilities_to_py_facade = "capabilities_to_py_facade.j2"
+capabilities_to_py_facade_out = f"{codetocad_dir}/facade/"
+
 
 capabilities_to_py_provider = "capabilities_to_py_provider.j2"
 capabilities_to_py_provider_out = f"{root_dir}/providers/sample"
