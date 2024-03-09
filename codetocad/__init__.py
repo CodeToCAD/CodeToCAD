4ad8fdd0483a4143ac207243e896b3c1dc259fd2
Shehab Attia <shehabattia96@gmail.com>
2024-03-08 19:04:27 -0500

    Added a method to generate a register.py for providers to auto-register themselves with the facade.

--- codetocad/__init__.py
+++ codetocad/__init__.py
@@ -2,17 +2,5 @@ from codetocad.utilities import *
 from codetocad.codetocad_types import *
 from codetocad.interfaces import *
 
-from importlib.util import find_spec
 
-if find_spec("blender_provider"):
-    # this will only be available when the Blender Addon is packaged:
-    from providers.blender.blender_provider import *
-elif find_spec("fusion360_provider"):
-    # this will only be available when the Fusion360 Addon is packaged:
-    from providers.fusion360.fusion360_provider import *
-else:
-    # When this python library is bundled, use the sample if non of the other known providers are available.
-    print(
-        "Warning: you are dry-running using the sample provider. All outputs are sample text explaining which methods will be executed. Please run this script in a supported software for a real output. See the README for more information."
-    )
-    from providers.sample import *
+from codetocad.facade import *
---
+++ providers/sample/register.py
@@ -0,0 +1,21 @@
+# THIS IS AN AUTO-GENERATED FILE. DO NOT CHANGE.
+
+from . import *
+
+
+def register():
+    __import__("codetocad").facade.entity.Entity.register(Entity)
+    __import__("codetocad").facade.part.Part.register(Part)
+    __import__("codetocad").facade.sketch.Sketch.register(Sketch)
+    __import__("codetocad").facade.vertex.Vertex.register(Vertex)
+    __import__("codetocad").facade.edge.Edge.register(Edge)
+    __import__("codetocad").facade.wire.Wire.register(Wire)
+    __import__("codetocad").facade.landmark.Landmark.register(Landmark)
+    __import__("codetocad").facade.joint.Joint.register(Joint)
+    __import__("codetocad").facade.material.Material.register(Material)
+    __import__("codetocad").facade.animation.Animation.register(Animation)
+    __import__("codetocad").facade.light.Light.register(Light)
+    __import__("codetocad").facade.camera.Camera.register(Camera)
+    __import__("codetocad").facade.render.Render.register(Render)
+    __import__("codetocad").facade.scene.Scene.register(Scene)
+    __import__("codetocad").facade.analytics.Analytics.register(Analytics)
