c7bb6d12f95ee665a879dd77a96ca1211e15c006
Shehab Attia <shehabattia96@gmail.com>
2024-05-04 13:21:34 -0400

    Updated pre-commit hook to run lint auto-fixing and autoformatting. Added an --autofix option to run_lint.sh

--- development/git_hooks/pre-commit.sh
+++ development/git_hooks/pre-commit.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
+# Run with -i to install this git hook
+
 ROOT_DIR=`git rev-parse --show-toplevel`
+GIT_HOOKS_DIR=`git rev-parse --git-dir`/hooks
+
+OUT_FILE="$GIT_HOOKS_DIR/pre-commit"
 
 echo "Running pre-commit hooks."
 
 set -e # exit if script errors.
+
+if [ $1 == "-i" ]; then
+    echo "Installing pre-commit hook"
+
+    cp "$ROOT_DIR/development/git_hooks/pre-commit.sh" $OUT_FILE
+
+    chmod +x $OUT_FILE
+    
+    echo "Installed successfully"
+
+    exit 0
+fi
+
 
 # Reference: https://prettier.io/docs/en/precommit.html
 # ACMR = Added, Copied, Modified, Renamed (no deleted)
@@ -12,7 +30,7 @@ FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep '\.\(py\)$' | se
 
 if [ ! -z "$FILES"]; then
     # Prettify all selected files
-    echo "$FILES" | xargs black
+    echo "$FILES" | xargs sh "$ROOT_DIR/development/run_lint.sh" --autofix
 
     # Add back the modified/prettified files to staging
     echo "$FILES" | xargs git add
@@ -20,7 +38,5 @@ fi
 
 # Mark: run tests  and lint checks
 sh "$ROOT_DIR/development/run_tests.sh"
-
-sh "$ROOT_DIR/development/run_lint.sh"
 
 echo "Done running pre-commit hooks."
