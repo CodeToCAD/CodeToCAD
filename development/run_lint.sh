65af96c6fee2234687fe681acef39c4e208c9ea6
Shehab Attia <shehabattia96@gmail.com>
2024-05-04 13:21:34 -0400

    Updated pre-commit hook to run lint auto-fixing and autoformatting. Added an --autofix option to run_lint.sh

--- development/run_lint.sh
+++ development/run_lint.sh
@@ -2,8 +2,19 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # copypasta from
 
 cd "$SCRIPT_DIR/.."
 
-autoflake --recursive -v --in-place --remove-all-unused-imports --exclude=__init__.py,codetocad_types.py,development/ codetocad providers examples tests tests_integration
+if [ $1 == "--autofix" ]; then
 
-black .
+    FILES="${@:2}"
+
+    echo "Autofixing $FILES" 
+
+    if [ -z $FILES ]; then
+        FILES="codetocad providers examples tests tests_integration"
+    fi
+
+    autoflake --recursive -v --in-place --remove-all-unused-imports --exclude=__init__.py,codetocad_types.py,development/ $FILES
+
+    black $FILES
+fi
 
 flake8 **/*.py
