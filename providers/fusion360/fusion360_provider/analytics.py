d16bcafe1ef051d89033d2befc8eec2613a59868
Shehab Attia <shehabattia96@gmail.com>
2024-08-01 22:11:26 -0400

    Updated the @supported decorators for the Fusion360 analytics adn animation classes.

--- providers/fusion360/fusion360_provider/analytics.py
+++ providers/fusion360/fusion360_provider/analytics.py
@@ -10,14 +10,14 @@ class Analytics(AnalyticsInterface):
     def __init__(self):
         pass
 
-    @supported(SupportLevel.UNSUPPORTED)
+    @supported(SupportLevel.PLANNED)
     def measure_distance(
         self, entity1: "str|EntityInterface", entity2: "str|EntityInterface"
     ) -> "Dimensions":
         print("measure_distance called:", entity1, entity2)
         return Dimensions.from_point(Point.from_list_of_float_or_string([0, 0, 0]))
 
-    @supported(SupportLevel.UNSUPPORTED)
+    @supported(SupportLevel.PLANNED)
     def measure_angle(
         self,
         entity1: "str|EntityInterface",
@@ -27,7 +27,7 @@ class Analytics(AnalyticsInterface):
         print("measure_angle called:", entity1, entity2, pivot)
         return [Angle(90)]
 
-    @supported(SupportLevel.UNSUPPORTED)
+    @supported(SupportLevel.PLANNED)
     def get_world_pose(self, entity: "str|EntityInterface") -> "list[float]":
         print("get_world_pose called:", entity)
         return [
@@ -49,17 +49,17 @@ class Analytics(AnalyticsInterface):
             1.0,
         ]
 
-    @supported(SupportLevel.UNSUPPORTED)
+    @supported(SupportLevel.PLANNED)
     def get_bounding_box(self, entity_name: "str|EntityInterface") -> "BoundaryBox":
         print("get_bounding_box called:", entity_name)
         return BoundaryBox(BoundaryAxis(0, 0), BoundaryAxis(0, 0), BoundaryAxis(0, 0))
 
-    @supported(SupportLevel.UNSUPPORTED)
+    @supported(SupportLevel.PLANNED)
     def get_dimensions(self, entity_name: "str|EntityInterface") -> "Dimensions":
         print("get_dimensions called:", entity_name)
         return Dimensions.from_point(Point.from_list_of_float_or_string([0, 0, 0]))
 
-    @supported(SupportLevel.UNSUPPORTED)
+    @supported(SupportLevel.PLANNED)
     def log(self, message: "str"):
         print("log called:", message)
         return self
