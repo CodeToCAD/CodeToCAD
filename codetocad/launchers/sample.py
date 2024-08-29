685e7a5ee7ab34f5431d6ed622030b4f8ebd7330
Shehab Attia <shehabattia96@gmail.com>
2024-08-29 10:08:12 -0400

    Fixed launches/sample.py not registering itself before running (#375)

--- codetocad/launchers/sample.py
+++ codetocad/launchers/sample.py
@@ -1,9 +1,12 @@
 import runpy
 from codetocad.launchers.launcher_args import LauncherArgs
 
+from providers.sample import register
+
 
 def run_with_sample(launcher_args: LauncherArgs):
     """
     Launches the script with the providers.sample module.
     """
+    register.register()
     return runpy.run_path(launcher_args.script_file_path, run_name="__main__")
