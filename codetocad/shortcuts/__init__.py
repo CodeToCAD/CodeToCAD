72b0ee8046c51b95d8dd296e9de85f41b7bab762
Shehab Attia <shehabattia96@gmail.com>
2024-10-12 10:27:51 -0400

    Added shortcuts. These will optionally export commonly used CodeToCAD enumerations to make scripting faster.

---
+++ codetocad/shortcuts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Exports commonly used enums, such as Axis min, max center as variable names.
+
+In other words, after using `from codetocad.shortcuts import *` the user can use `min + "5mm"` to denote 5mm from the axis minimum.
+"""
+
+from codetocad.shortcuts.axis import *
---
+++ codetocad/shortcuts/axis.py
@@ -0,0 +1,16 @@
+from codetocad.enums.axis import Axis
+
+
+X = Axis.X
+Y = Axis.Y
+Z = Axis.Z
+x = Axis.X
+y = Axis.Y
+z = Axis.Z
+
+MIN = Axis.MIN
+MAX = Axis.MAX
+CENTER = Axis.CENTER
+min = Axis.MIN
+max = Axis.MAX
+center = Axis.CENTER
