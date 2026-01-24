6f09b349708c041c873d41850d1bfb9aa3f08794
Shehab Attia <shehabattia96@gmail.com>
2026-01-24 01:53:21 -0500

    Added a Sketch dataclass

--- codetocad/core/cad/sketch.py
+++ codetocad/core/cad/sketch.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
-import math
-
 from codetocad.core.cad.native import NativeObject
-from codetocad.core.dimensions.angle import Angle, AngleType
-from codetocad.core.dimensions.length_expression import LengthExp, LengthType
-from codetocad.core.cad.vertex_edge_solid import CurveType, Edge, Vertex
-from codetocad.core.enums.plane import Plane
 
 
-@dataclass
+@dataclass(kw_only=True)
 class Sketch(NativeObject):
-    """A sketch is a 2D drawing in a plane."""
+    """A vertex is a point in 3D space."""
+
+    is_hidden: bool = False
+
+
+def get_sketch(sketch_name: str) -> "Sketch":
+    """Get a sketch by name."""
+    raise NotImplementedError("Method not implemented.")
